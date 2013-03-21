f36ed658d8025bbfd5118b702a50af2457092564
Joseph Feeney <joefeeney@me.com>
2013-03-21 16:06:15 -0700

    Staging for new suite of scripts

---
+++ bin/adapterTrackerMonthlyParser.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import csv
+import subprocess
+from collections import defaultdict
+import re
+
+def illegal_char_replace(string):
+    illegal_bcl_characters = """ ?()[]\=+<>:;"',*|^&`"""
+
+    string = string.replace("/","_").replace("-","_").replace(".","_").replace(" ","_")
+
+    return string
+
+if __name__=="__main__":
+    
+    try:
+        if not os.path.isfile(sys.argv[1]):
+            print("CSV file doesn't exist")
+            sys.exit(1)
+
+        csv_file = sys.argv[1]
+
+    except IndexError:
+        print("")
+        print("adapterTrackerMonthlyParser.py <Monthly CSV Adapter Track file>")
+        print("")
+        
+        sys.exit(1)
+
+
+    runs = defaultdict(list)
+
+    # get Barcodes
+    barcodes_path = os.path.split(sys.argv[0])[0] + "/barcodes"
+
+    if os.path.isfile(barcodes_path):
+
+        barcodes = {}
+        barcodes_sideA = {}
+        barcodes_sideB = {}
+
+        with open(barcodes_path,"r") as barcodes_file:
+            for line in barcodes_file:
+
+                row = line.strip().split()
+                
+                side  = row[0] 
+                key   = row[1]
+                value = row[2]
+
+                if side == "A":
+                    barcodes_sideA[key] = value
+
+                elif side == "B":
+                    barcodes_sideB[key] = value
+
+                barcodes[key] = value 
+
+
+    # Parse the runs in the file
+    with open(csv_file,"rU") as csvfile:
+
+        adapter_month = csv.reader(csvfile)
+
+        for row in adapter_month:
+
+            run_name = row[11]
+
+            if run_name == "" or run_name == "Run":
+                continue
+
+            runs[run_name].append(row)
+
+    # Loop through runs and Create Sample Sheets
+
+    for k in runs.keys():
+
+        with open("SampleSheet_" + k + ".csv","w") as sample_sheet_out:
+
+            sample_sheet_out.write("FCID,Lane,Sample_ID,Sample_Ref,Index,Descripton,Control,Recipe,Operator,Sample_Project\n")
+
+            for row in runs[k]:
+
+                owner            = row[1]
+                email_address    = row[2]
+                illumina_adapter = illegal_char_replace(row[3])
+                project          = illegal_char_replace(row[4])
+                sample_name      = illegal_char_replace(row[5])
+                illumina_adapter_sequence = row[6]
+                bowtie           = row[8]
+                mysql_host       = row[9]
+                mysql_database   = row[10]
+                lane             = row[12]
+                barcode1         = row[13]
+                barcode2         = row[14]
+
+                if barcode1 in barcodes_sideA:
+                    barcode1_sequence = barcodes_sideA[barcode1]
+                
+                elif len(barcode1) > 0 and barcode1 not in barcodes_sideA:
+                    # If the barcode isn't associated with Ronan's Barcode's file and
+                    # is directly given in the Illumina Adapter Tracker
+                    barcode1_sequence = barcode1
+                else:
+                    barcode1_sequence = ""
+
+
+                if barcode2 in barcodes_sideB:
+                    barcode2_sequence = barcodes_sideB[barcode2]
+
+                elif len(barcode2) > 0 and barcode2 not in barcodes_sideB:
+                    barcode2_sequence = barcode2
+
+                else:
+                    barcode2_sequence = ""
+
+
+                sample_sheet_out.write(",".join(["",lane,sample_name,illumina_adapter,illumina_adapter_sequence,bowtie + ";" + mysql_host + ";" + mysql_database + ";"
+                                                  + barcode1_sequence + ";" + barcode2_sequence,"","",email_address,project + "\n"]))

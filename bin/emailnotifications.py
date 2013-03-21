fee0b88a430ec62541db09a3b0d60669804a9c2f
Joseph Feeney <joefeeney@me.com>
2013-03-21 15:41:21 -0700

    Added JN to admins

--- bin/emailnotifications.py
+++ bin/emailnotifications.py
@@ -7,8 +7,8 @@ class notifications(object):
         self.password = "g3n0m3analysis"
         self.FROM     = "genomic.analysis.ecker@gmail.com"
 
-        #self.admin    = ["jfeeneysd@gmail.com","jnery@salk.edu"]
-        self.admin = ["jfeeneysd@gmail.com"]
+        self.admin    = ["jfeeneysd@gmail.com","jnery@salk.edu"]
+        # self.admin = ["jfeeneysd@gmail.com"]
 
     def send_message(self,TO,SUBJECT,TEXT):
         """
